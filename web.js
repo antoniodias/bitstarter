e6e8fd46f4444cbed070eb1ad89637bfb261ca42
antoniodias <antoniolvdias@gmail.com>
2013-07-12 11:00:00 +0000

    Update web.js

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    fs.readFile('/index.html', function (err, data){
+	if (err) throw err;
+	console.log(buf.toString('utf8','0', data));
 });
 
 var port = process.env.PORT || 5000;
